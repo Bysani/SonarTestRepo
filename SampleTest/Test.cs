aaae8f3cc1f35b7d8b1082b9ee7ec162580c0767
Bysani <bchaithanya@opteamix.com>
2019-12-30 17:55:31 +0530

    First Commit

---
+++ SampleTest/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace SampleTest
+{
+    
+    public class Test
+    {
+        [Fact]
+        public void AddTwoNumbers()
+        {
+            int a = 5;
+            int b = 5;
+            Assert.True(a+b>0);
+        }
+    }
+}
---
+++ XUnitTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void AddTwoNumbers()
+        {
+            int a = 5;
+            int b = 5;
+            Assert.True(a + b > 0);
+        }
+    }
+}
